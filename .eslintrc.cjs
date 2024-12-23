fe0614bf1908d6c44a88536d56ddc0c1c1fd9cc3
Dmitriy Slugin <dslugin@ntdev.ru>
2024-12-23 17:57:04 +0300

    added eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,91 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  extends: [
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:prettier/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', '@stylistic/eslint-plugin', 'import'],
+  rules: {
+    'prettier/prettier': 'error',
+    '@stylistic/indent': ['error', 2],
+    '@stylistic/quotes': ['error', 'single'],
+    '@stylistic/quote-props': ['error', 'consistent-as-needed'],
+    '@stylistic/key-spacing': [
+      'error',
+      { beforeColon: false, afterColon: true, mode: 'strict' },
+    ],
+    '@stylistic/type-annotation-spacing': 'error',
+    '@stylistic/padding-line-between-statements': [
+      'error',
+      { blankLine: 'always', prev: '*', next: 'return' },
+      { blankLine: 'always', prev: '*', next: 'function' },
+      { blankLine: 'always', prev: 'function', next: '*' },
+    ],
+    '@stylistic/member-delimiter-style': [
+      'error',
+      {
+        multiline: {
+          delimiter: 'semi',
+          requireLast: true,
+        },
+        singleline: {
+          delimiter: 'semi',
+          requireLast: false,
+        },
+      },
+    ],
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'react/jsx-filename-extension': [
+      2,
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    ],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'max-len': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { args: 'none' }],
+    'class-methods-use-this': 'off',
+    'import/prefer-default-export': 'off',
+    'no-param-reassign': ['error', { props: false }],
+    'react/react-in-jsx-scope': 'off',
+    'react/function-component-definition': 'off',
+    'jsx-a11y/label-has-associated-control': 'off',
+    'react/jsx-first-prop-new-line': ['error', 'multiprop'],
+    'react/require-default-props': 'off',
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': ['error'],
+    'react/jsx-props-no-spreading': 'off',
+    '@typescript-eslint/consistent-type-imports': 'error',
+    'no-empty': 'off',
+    'quote-props': 'off',
+    'function-paren-newline': ['error', { minItems: 4 }],
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {}, 
+    },
+  },
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
+
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: chunkInfo => {
+          const isCss = chunkInfo.names?.includes('.css')
+          return isCss ? `assets/[name]-[hash].[ext]` : `assets/[name].[ext]`
+        },
+      },
+    },
+  },
+
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@modules': path.resolve(__dirname, './src/shared/modules'),
+      '@utils': path.resolve(__dirname, './src/shared/utils'),
+      '@app': path.resolve(__dirname, './src/app'),
+      '@entities': path.resolve(__dirname, './src/entities'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@features': path.resolve(__dirname, './src/features'),
+      '@widgets': path.resolve(__dirname, './src/widgets'),
+      '@shared': path.resolve(__dirname, './src/shared'),
+    },
+  },
 });
